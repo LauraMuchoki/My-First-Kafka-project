2294d7418cfc1ee456d0696794de48949c250705
kafka <laurynemuchoki@gmail.com>
2021-10-28 10:53:09 +0300

    My first Commit

--- src/main/java/com/github/simplelaura/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/github/simplelaura/kafka/tutorial2/TwitterProducer.java
@@ -2,6 +2,7 @@ package com.github.simplelaura.kafka.tutorial2;
 
 public class TwitterProducer {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
     }
 }
